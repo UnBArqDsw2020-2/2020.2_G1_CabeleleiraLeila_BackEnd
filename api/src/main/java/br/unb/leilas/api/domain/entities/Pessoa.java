041aef4c7f3d7089448e0dbde7d0c30fd309d1dc
geraldovictor <geraldovictor@outlook.com>
2021-04-29 07:03:06 -0300

    Adiciona getters and setters faltantes

--- api/src/main/java/br/unb/leilas/api/domain/entities/Pessoa.java
+++ api/src/main/java/br/unb/leilas/api/domain/entities/Pessoa.java
@@ -5,70 +5,95 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Inheritance(strategy =  InheritanceType.JOINED)
-@DiscriminatorColumn(name = "tipo", length = 2, discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(
+  name = "tipo",
+  length = 2,
+  discriminatorType = DiscriminatorType.STRING
+)
 @DiscriminatorValue("P")
 @Table(name = "pessoa")
+public class Pessoa extends BaseEntity {
 
-public class Pessoa extends BaseEntity{
-    private String nome;
-    private LocalDate nascimento;
-    private String telefone;
-    private String rg;
-    private String rgEmissor;
-    @Column(unique = true)
-    private String cpf;
-    @Column(insertable=false, updatable=false) // atributo tipo é realmente inalterável?
-    private String tipo;
-  
-    @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "autenticacao_id", referencedColumnName = "id")
-    private Autenticacao autenticacao;
+  private String nome;
+  private LocalDate nascimento;
+  private String telefone;
+  private String rg;
+  private String rgEmissor;
 
-    public String getNome() {
-        return nome;
-    }
+  @Column(unique = true)
+  private String cpf;
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  @Column(insertable = false, updatable = false) // atributo tipo é realmente inalterável?
+  private String tipo;
 
-    public LocalDate getNascimento() {
-        return nascimento;
-    }
+  @OneToOne(cascade = CascadeType.PERSIST)
+  @JoinColumn(name = "autenticacao_id", referencedColumnName = "id")
+  private Autenticacao autenticacao;
 
-    public void setNascimento(LocalDate nascimento) {
-        this.nascimento = nascimento;
-    }
+  public String getTelefone() {
+    return this.telefone;
+  }
 
-  // private LocalDate nascimento;
-  // private String telefone;
-  // private String rg;
-  // private String rgEmissor;
-  // @Column(unique = true)
-  // private String cpf;
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
 
-  // @Transient ignora ao salvar e é retornado nas requisições
+  public String getRg() {
+    return this.rg;
+  }
 
-  // @JsonIgnore ignora ao ser passado como json nas requisições mas permite
-  // persistir
+  public void setRg(String rg) {
+    this.rg = rg;
+  }
 
-  public String getTipo() {  
+  public String getRgEmissor() {
+    return this.rgEmissor;
+  }
+
+  public void setRgEmissor(String rgEmissor) {
+    this.rgEmissor = rgEmissor;
+  }
+
+  public String getCpf() {
+    return this.cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public LocalDate getNascimento() {
+    return nascimento;
+  }
+
+  public void setNascimento(LocalDate nascimento) {
+    this.nascimento = nascimento;
+  }
+
+  public String getTipo() {
     return tipo;
   }
 
