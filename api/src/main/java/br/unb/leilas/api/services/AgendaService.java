9c52faaf9a1bf0fd6f7f223a8502d4eff37a748b
KalebeLopes <calbkalebe@gmail.com>
2021-04-29 18:11:16 -0300

    implementacao do metodo getAgendaPorDatas
    Co-authored-by: Damarcones Porto <damarcones@gmail.com>
    Co-authored-by: Erick Giffoni <giffoni.erick@gmail.com>
    Co-authored-by: Bruna Almeida <brunaalmeida48@gmail.com>

--- api/src/main/java/br/unb/leilas/api/services/AgendaService.java
+++ api/src/main/java/br/unb/leilas/api/services/AgendaService.java
@@ -1,31 +1,71 @@
 package br.unb.leilas.api.services;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
 
 import br.unb.leilas.api.domain.entities.Agenda;
 import br.unb.leilas.api.repositories.AgendaRepository;
 
+@Service
 public class AgendaService {
 
    private AgendaRepository agendaRepository;
 
-   public Agenda[] getAgendasPorData(String dataInicio, String dataFim) {
-       // buscar no intervalo dataInicio - dataFim as agendas
+   public AgendaService(AgendaRepository agendaRepository) {
+      this.agendaRepository = agendaRepository;
+   }
+
+   public Iterable<Agenda> findAll() {
+     return this.agendaRepository.findAll();
+   }
+
+   public List<Agenda> getAgendasPorData(String dataInicio, String dataFim) {
+      List<Agenda> agenda = this.agendaRepository.findByData(dataInicio);
+      System.out.println(agenda);
+
+      return agenda;
+   }
+
+   public Map<String, String[]> getDiasEHorarios(String dataInicio, String dataFim, String nomeServico) throws ParseException {
+      // DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+      // Date inicio = formatter.parse(dataInicio);
+      // System.out.println(inicio);
+      // List<Agenda> agenda = this.agendaRepository.findByData(dataInicio);
+      // System.out.println(agenda);
       return null;
    }
 
-   public Map<String, String[]> getDiasEHorarios(Agenda[] agendas, String nomeServico) {
-      // para cada agenda (data), verificar o array de Pedidos
-         // para cada Pedido, verificar o array de Servico e
-         // filtrar pelo nomeServico
-
-         // verificar os horarios indisponiveis para saber os disponiveis
-
-         // adicionar os disponiveis ao array de horarios disponiveis
-
-      // retornar o resultado
+   public Agenda save(Agenda agenda) {
       
-      return null;
-   }
+      return this.agendaRepository.save(agenda);
+    }
+
+   // public Agenda[] getAgendasPorData(String dataInicio, String dataFim) {
+   //     // buscar no intervalo dataInicio - dataFim as agendas
+   //    return null;
+   // }
+
+   // public Map<String, String[]> getDiasEHorarios(Agenda[] agendas, String nomeServico) {
+   //    // para cada agenda (data), verificar o array de Pedidos
+   //       // para cada Pedido, verificar o array de Servico e
+   //       // filtrar pelo nomeServico
+
+   //       // verificar os horarios indisponiveis para saber os disponiveis
+
+   //       // adicionar os disponiveis ao array de horarios disponiveis
+
+   //    // retornar o resultado
+      
+   //    return null;
+   // }
+   
+
    
 }
