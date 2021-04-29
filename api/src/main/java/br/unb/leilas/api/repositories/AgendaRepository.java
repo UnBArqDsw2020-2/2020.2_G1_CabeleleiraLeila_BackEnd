c94be6eb3e1bc913f2945bfcbf62c9ba7b93ef83
KalebeLopes <calbkalebe@gmail.com>
2021-04-29 18:09:03 -0300

    atualizacao tipo do filtro
    Co-authored-by: Erick Giffoni <giffoni.erick@gmail.com>
    Co-authored-by: Bruna Almeida <brunaalmeida48@gmail.com>
    Co-authored-by: Damarcones Porto <damarcones@gmail.com>

--- api/src/main/java/br/unb/leilas/api/repositories/AgendaRepository.java
+++ api/src/main/java/br/unb/leilas/api/repositories/AgendaRepository.java
@@ -1,12 +1,14 @@
 package br.unb.leilas.api.repositories;
 
+import br.unb.leilas.api.domain.entities.Agenda;
 import java.util.Date;
 import java.util.List;
-
+import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
-import br.unb.leilas.api.domain.entities.Agenda;
-
+@Repository
 public interface AgendaRepository extends CrudRepository<Agenda, Integer> {
-   List<Agenda> findByData(Date data);
+   
+   List<Agenda> findByData(String data);
+   
 }
