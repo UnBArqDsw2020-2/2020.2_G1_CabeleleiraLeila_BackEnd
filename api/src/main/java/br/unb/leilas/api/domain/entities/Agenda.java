2582230f1ce294e9e91165aed7a5c71533f934cc
KalebeLopes <calbkalebe@gmail.com>
2021-04-29 18:07:03 -0300

    atualizacao classe agenda
    Co-authored-by: Erick Giffoni <giffoni.erick@gmail.com>
    Co-authored-by: Damarcones Porto <damarcones@gmail.com>
    Co-authored-by: Bruna Almeida <brunaalmeida48@gmail.com>

--- api/src/main/java/br/unb/leilas/api/domain/entities/Agenda.java
+++ api/src/main/java/br/unb/leilas/api/domain/entities/Agenda.java
@@ -3,33 +3,54 @@ package br.unb.leilas.api.domain.entities;
 import java.util.Date;
 
 import br.unb.leilas.api.domain.entities.base.BaseEntity;
+// import br.unb.leilas.api.domain.entities.Pedido;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.DiscriminatorType;
+
+import javax.persistence.Table;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorValue(value = "S")
+@Table(name = "agenda")
 
 public class Agenda extends BaseEntity {
-   private Pedido[] pedidos;
-   private Date date;
+   // private Pedido[] pedidos;
+   @Column(unique = true)
+   private String data;
 
    public Agenda(){
-      this.date = new Date();
+      this.data = new Date().toString();
    }
 
-   public Agenda(Date data){
-      setDate(data);
+   // public Agenda(){
+   //    this.date = new Date();
+   // }
+
+   // public Agenda(Date data){
+   //    setDate(data);
+   // }
+
+   // public Pedido[] getPedidos() {
+   //    return pedidos;
+   // }
+
+   // public void setPedidos(Pedido[] pedidos) {
+   //    this.pedidos = pedidos;
+   // }
+
+   public String getDate() {
+      return data;
    }
 
-   public Pedido[] getPedidos() {
-      return pedidos;
-   }
-
-   public void setPedidos(Pedido[] pedidos) {
-      this.pedidos = pedidos;
-   }
-
-   public Date getDate() {
-      return date;
-   }
-
-   public void setDate(Date date) {
-      this.date = date;
+   public void setDate(String data) {
+      this.data = data;
    }
    
 }
